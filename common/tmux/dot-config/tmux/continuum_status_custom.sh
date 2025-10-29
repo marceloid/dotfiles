5fe61c43b13c9a9300589b9a5e18644306f4e799
João Marcelo <joaomarceloav@gmail.com>
2025-10-29 10:32:33 -0300

    fix: Update last_saved_time continuum script to work both in linux and macos

--- common/tmux/dot-config/tmux/continuum_status_custom.sh
+++ common/tmux/dot-config/tmux/continuum_status_custom.sh
@@ -1,7 +1,26 @@
+##!/bin/bash
+#
+#last_save_file="$HOME/.local/share/tmux/resurrect/last"
+#
+#if [ -f "$last_save_file" ]; then
+#  # Detect OS
+#  if [[ "$OSTYPE" == "darwin"* ]]; then
+#    # macOS uses BSD stat
+#    last_save_time=$(stat -f "%Sm" -t "%H:%M" "$last_save_file")
+#  else
+#    # Linux uses GNU stat
+#    last_save_time=$(stat -c "%y" "$last_save_file" | awk '{print substr($2,1,5)}')
+#  fi
+#  echo "💾 last saved $last_save_time"
+#else
+#  echo "💾 off"
+#fi
 #!/bin/bash
+
 last_save_file="$HOME/.local/share/tmux/resurrect/last"
+
 if [ -f "$last_save_file" ]; then
-  last_save_time=$(stat -f "%Sm" -t "%H:%M" "$last_save_file")
+  last_save_time=$(date -r "$last_save_file" +"%H:%M" 2>/dev/null || date -d @"$(stat -c %Y "$last_save_file")" +"%H:%M")
   echo "💾 last saved $last_save_time"
 else
   echo "💾 off"
