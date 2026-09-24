ca60c83051a0eded29a11427d2e468fb97ae1340
João Marcelo <joaomarceloav@gmail.com>
2026-09-23 21:19:25 -0300

    nvim: abrir dashboard ao invés do file tree ao iniciar em diretório

--- common/nvim/dot-config/nvim/lua/config/autocmds.lua
+++ common/nvim/dot-config/nvim/lua/config/autocmds.lua
@@ -3,6 +3,20 @@
 --
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Ao abrir o Neovim passando um diretório (ex: `nvim .`), abre o Dashboard em vez do file tree
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("snacks_dashboard_on_dir", { clear = true }),
+  callback = function()
+    if vim.fn.argc(-1) == 1 then
+      local arg = vim.fn.argv(0)
+      if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+        vim.schedule(function()
+          if Snacks and Snacks.dashboard then
+            Snacks.dashboard.open()
+          end
+        end)
+      end
+    end
+  end,
+})
---
+++ common/nvim/dot-config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        replace_netrw = false, -- Não abre o explorer automaticamente ao abrir diretórios
+      },
+    },
+  },
+}
